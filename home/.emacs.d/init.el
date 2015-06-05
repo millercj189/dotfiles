e56bb70ddf5362b945ca457edf7900092a3299c1
mdln <maudlin.johnathan@gmail.com>
2015-06-05 03:03:00 -0400

    Restore

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,8 +1,8 @@
 ;; Settings
 ;;----------------------------------------------------------
-;(tool-bar-mode -1)
-;(menu-bar-mode -1)
-;(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 
